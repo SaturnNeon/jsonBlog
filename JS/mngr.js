5c8f8b04eb082b5f9f66c249a4c97d38e8c02fe1
PixelNetNeon <neonisdaboi@outlook.com>
2024-04-01 10:41:43 -0500

    what am i even doing at this point

--- JS/mngr.js
+++ JS/mngr.js
@@ -10,7 +10,7 @@ xhttp.onreadystatechange = function() {
         parsedTable = JSON.parse(blogs)
 
         for (var i in parsedTable) {
-            console.log(parsedTable.blogFile[i].file)
+            console.log(parsedTable.txt[i])
         };
     } 
   }; 
