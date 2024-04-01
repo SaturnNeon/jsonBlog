6efb15952425a2da2461b38828244edc7103b2c3
PixelNetNeon <neonisdaboi@outlook.com>
2024-04-01 10:08:00 -0500

    json tester

--- JS/mngr.js
+++ JS/mngr.js
@@ -1,1 +1,13 @@
-// TBD
+var table = "./blogList.json"
+
+var xhttp = new XMLHttpRequest(); 
+xhttp.onreadystatechange = function() { 
+    if (this.readyState == 4 && this.status == 200) { 
+        var blogs = this.responseText; 
+        
+        console.log(JSON.parse(blogs))
+    } 
+  }; 
+
+xhttp.open("GET", table, true); 
+xhttp.send(); 
