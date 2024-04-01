b18c2f1c68db323d22f0152474552bac9639b695
PixelNetNeon <neonisdaboi@outlook.com>
2024-04-01 09:30:25 -0500

    test 2: electric boogaloo

--- JS/readBlog.js
+++ JS/readBlog.js
@@ -1,9 +1,12 @@
+// Blog Reading Code
 var xhttp = new XMLHttpRequest(); 
+
 xhttp.onreadystatechange = function() { 
   if (this.readyState == 4 && this.status == 200) { 
     var text = this.responseText; 
     console.log(text)
   } 
 }; 
-xhttp.open("GET", "./blogtxt/test.txt", true); 
+
+xhttp.open("GET", "../blogtxt/test.txt", true); 
 xhttp.send(); 
