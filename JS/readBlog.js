5e2dd6ca558fa32c8ceed7ab016e26b8204b7213
PixelNetNeon <neonisdaboi@outlook.com>
2024-04-01 09:45:52 -0500

    slowly getting there!

--- JS/readBlog.js
+++ JS/readBlog.js
@@ -4,6 +4,7 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() { 
   if (this.readyState == 4 && this.status == 200) { 
     var text = this.responseText; 
+    document.getElementById("posttext").innerHTML = text;
     console.log(text)
   } 
 }; 
