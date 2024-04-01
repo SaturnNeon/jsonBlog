1c7d3f53f46b2a81d85efba0cc158048893f5574
PixelNetNeon <neonisdaboi@outlook.com>
2024-04-01 09:53:42 -0500

    experimental thing

--- JS/readBlog.js
+++ JS/readBlog.js
@@ -1,6 +1,9 @@
 // Blog Reading Code
 var xhttp = new XMLHttpRequest(); 
 
+const urlParams = new URL(window.location.toLocaleString()).searchParams;
+const postTXT = urlParams.get('post');
+
 xhttp.onreadystatechange = function() { 
   if (this.readyState == 4 && this.status == 200) { 
     var text = this.responseText; 
@@ -9,5 +12,5 @@ xhttp.onreadystatechange = function() {
   } 
 }; 
 
-xhttp.open("GET", "./posts/test.txt", true); 
+xhttp.open("GET", "./posts/"+ postTXT +".txt", true); 
 xhttp.send(); 
